4cde8742685d27238caad7e877862edd1705aad9
mareun <44865268+mareun@users.noreply.github.com>
2020-02-20 14:36:29 +0900

    02/20

--- study/Android/Practice03/app/src/main/java/com/example/practice03/FirstFragment.kt
+++ study/Android/Practice03/app/src/main/java/com/example/practice03/FirstFragment.kt
@@ -6,43 +6,47 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.rv_data_list_item.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class FirstFragment : Fragment() {
 
     val dataArray: ArrayList<String> = ArrayList()
-
-    private fun addDataArray(){
-        dataArray.add("1")
-        dataArray.add("2")
-        dataArray.add("3")
-        dataArray.add("4")
-        dataArray.add("5")
-        dataArray.add("6")
-        dataArray.add("7")
-        dataArray.add("8")
-        dataArray.add("9")
-        dataArray.add("10")
-        dataArray.add("11")
-        dataArray.add("12")
-        dataArray.add("13")
-        dataArray.add("14")
-        dataArray.add("15")
-        dataArray.add("16")
-        dataArray.add("17")
-        dataArray.add("18")
-        dataArray.add("19")
-        dataArray.add("20")
-    }
+    lateinit var recyclerView01: RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var rootView = inflater.inflate(R.layout.rv_data_list_item, container, false)
         addDataArray()
-        rv_data_list.layoutManager = LinearLayoutManager(this)
-        rv_data_list.adapter = DataAdapter(dataArray, this)
+        recyclerView01 = rootView.findViewById(R.id.rv_data_list)as RecyclerView
+        recyclerView01.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView01.adapter = DataAdapter(dataArray, requireContext())
 
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return rootView
 
     }
-
-
+    private fun addDataArray(){
+        dataArray.add("여기에")
+        dataArray.add("배열의")
+        dataArray.add("문자열")
+        dataArray.add("알아서")
+        dataArray.add("변환해서")
+        dataArray.add("들어가기")
+        dataArray.add("그랬으면")
+        dataArray.add("좋겠다")
+        dataArray.add("졸작")
+        dataArray.add("화이팅")
+        dataArray.add("실행결과")
+        dataArray.add("조금만더")
+        dataArray.add("써서")
+        dataArray.add("스크롤")
+        dataArray.add("밑으로")
+        dataArray.add("내려가는지")
+        dataArray.add("봐야지")
+        dataArray.add("그래야")
+        dataArray.add("잘")
+        dataArray.add("보이지")
+    }
 }
